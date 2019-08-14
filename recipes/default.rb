c26d11c304b11c03191271d818fb5b28a94ab3a3
Matthew Mackreth <MMackreth@spartaglobal.com>
2019-08-14 10:42:18 +0100

    Added updating of source list for correct mongo version

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+apt_update 'update_sources' do
+  action :update
+end
+apt_repository 'mongodb-org' do
+  uri 'http://repo.mongodb.org/apt/ubuntu'
+  distribution 'trusty/mongodb-org/3.2'
+  components ['multiverse']
+  keyserver 'hkp://keyserver.ubuntu.com:80'
+  key 'EA312927'
+  action :add
+end
+
+
+package 'mongodb-org' do
+  action [:install, :upgrade]
+end
